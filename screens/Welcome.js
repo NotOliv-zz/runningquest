35d21acbcaeaa2215227eb489576bc2670445c31
FlorentH-12 <hirel.florent@hotmail.fr>
2021-05-17 17:32:19 +0200

    progression bar

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,21 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
 
-import {Button, Input, LinearProgress} from 'react-native-elements'
+import {Button, Input, LinearProgress, ListItem} from 'react-native-elements'
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Welcome(props) {
   return (
     <View style={styles.container}>
-      <View style={progress.container}>
-        <Text>Bienvenue sur </Text>
+      <ListItem >
+      <View style={swipe.container}>
+        <Text style={{ fontSize : 25}}>Bienvenue sur </Text>
         <Image 
-          style={{ width: 100, height: 100}}
+          style={{ width: 165, height: 100}}
           source={require("../assets/Logo.png")}></Image>
-        <Text>Vous êtes nouveau ici ?</Text>
-        <Text>Rejoignez nous et commencez plus attendre !</Text>
-        <LinearProgress style={{trackColor: "#ED590C"}}/>
+        <Text style={{ fontSize : 15}}>Vous êtes nouveau ici ?</Text>
+        <Text style={{ fontSize : 15, textAlign: 'center'}}>Rejoignez nous et commencez sans plus attendre !</Text>
+        <LinearProgress style={
+          color = "#ED590C",
+          value = "1"
+        }
+        />
       </View>
+      </ListItem>
       <StatusBar style="auto" />
       <View style={button.container}>
         <Button
@@ -42,14 +49,20 @@ export default function Welcome(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C4C4C4',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
 
-const progress = StyleSheet.create({
+const swipe = StyleSheet.create({
   container:{
+    flexDirection: 'column',
+    borderWidth: 20,
+    borderColor: '#C4C4C4',
+    alignItems: 'center',
+    backgroundColor: '#C4C4C4'
+
   }
 
 })
@@ -57,6 +70,7 @@ const progress = StyleSheet.create({
 const button = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    margin: 5,
   }
 })
 
