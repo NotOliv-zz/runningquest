e8031c009ad2b1217d56d813101ded933fb074c3
JLN <nguyen.jluc@gmail.com>
2021-05-17 19:11:49 +0200

    JLN 3eme Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useLayoutEffectn, useState } from 'react';
-import { StyleSheet, Text, View, Image, Picker } from 'react-native';
+import { StyleSheet, Text, View, Image, Picker, ScrollView, SafeAreaView } from 'react-native';
+import Navheader from "../component/Navheader";
 
 
 export default function Home() {
@@ -10,54 +11,51 @@ console.log(selectedValue)
   var runner = ["Adeline","Florent","Olivier","Jean-Luc"]
 
   var ranking = runner.map((runner,i)=>{
-    return (<Text>{i+1} - {runner}</Text>)
+    return (<Text key={i}>{i+1} - {runner}</Text>)
   }
 
   )
 
-
-
-
   return (
-    <View style={{flex :1, alignItems: 'center'}}>
-     
-
-      <Image style={{ width: '80%', height: '50%', marginBottom: 10, marginTop: 30 }}
         
-          source={require('../assets/Map-Levallois.jpg')}
-      />
-      
-      <View>
-        <Text style={{ margin: 10 }}>10% d'exploration</Text>
+    <View style={styles.container}>
 
-        
-      </View>
+      <Navheader/>
+      <Text>Home</Text>
       
-    <View style={styles.view}>
-      <Text> Localisation </Text>
-      <Picker
-        selectedValue = {selectedValue}
-        onValueChange ={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        style={{ height: 50, width: 150 }}
-        >
-          <Picker.Item label="Reims" value="Reims"/>
-          <Picker.Item label="Paris" value="Paris"/>
-      </Picker>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image style={styles.img}
+            
+              source={require('../assets/Map-Levallois.jpg')}
+          />
+        
+          <View>
+            <Text style={{ margin: 10, justifyContent: 'center'}}>10% d'exploration</Text>
+          </View>
+        
+          <View style={styles.view}>
+            <Text style={{fontWeight: "bold"}}> Localisation </Text>
+            <Picker
+              selectedValue = {selectedValue}
+              onValueChange ={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+              style={{ height: 40, width: 150 }}
+              >
+                <Picker.Item label="Reims" value="Reims"/>
+                <Picker.Item label="Paris" value="Paris"/>
+            </Picker>
+          </View>
+
+          <View style={styles.view}>
+            <Text style={{fontWeight: "bold"}} >Classement de ta ville Top 3</Text>
+              {ranking}
+            <Text>3555 - Jean-Luc</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
-
-
-
+   
     
-      <View style={styles.view}>
-        <Text >Classement de ta ville Top 3</Text>
-      {ranking}
-      <Text>3555 - Jean-Luc</Text>
-      </View>
-
-      <StatusBar style="auto" />
-
-
-    </View>
   );
 }
 
@@ -69,17 +67,29 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
   view :{
     textAlign:'center',
     alignItems: 'center',
     borderRadius: 10,
-    borderColor: '#fff',
+    borderColor: '#f0f0f0',
     borderWidth: 1,
     padding : 10,
-    width : '60%',
-    marginBottom : 10
+    width : '70%',
+    marginBottom : 10,
+    backgroundColor: '#fff',
 
 
+  },
+  
+      img: {
+        borderColor: '#f0f0f0',
+        borderWidth: 1,
+        width: 300, 
+        height: 250,
+        marginBottom: 10,
+        marginTop: 10 
+     
   },
 });
