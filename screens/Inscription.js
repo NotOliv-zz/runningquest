91eb313a166b06c710930f16a52a140a582af367
FlorentH-12 <hirel.florent@hotmail.fr>
2021-05-19 17:22:53 +0200

    Mise en place du ranking

--- screens/Inscription.js
+++ screens/Inscription.js
@@ -20,7 +20,7 @@ function Inscription(props) {
 
 var handleSubmitSignUp = async () => {
       
-  const data = await fetch('http://192.168.1.23:3000/sign-up', {
+  const data = await fetch('http://192.168.0.13:3000/sign-up', {
     method: 'POST',
     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
     body: `pseudoFromFront=${signUpPseudo}&emailFromFront=${signUpEmail}&passwordFromFront=${signUpPassword}`
@@ -80,3 +80,70 @@ export default connect(
   null,
   mapDispatchToProps
   )(Inscription)
+
+/* import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import {Button, Input, Icon} from 'react-native-elements'
+
+export default function Inscription(props) {
+  return (
+
+    <View style={styles.container}>
+
+    <View style={styles.Input}>
+
+    <Input
+      placeholder='Pseudo'
+      leftIcon={
+        <Icon name='user' type='font-awesome' size={24} color='black' />
+      }
+      />
+
+    <Input
+      placeholder='Email'
+      leftIcon={
+        <Icon
+        name='at'
+        type='font-awesome'
+        size={24}
+        color='black'
+        />
+      }
+    />
+
+    <Input
+        placeholder='Password'
+        secureTextEntry={true} 
+        leftIcon={
+          <Icon
+          name="unlock-alt"
+          type='font-awesome'
+          size={24}
+          color='black'
+          />
+        }       
+    />
+      </View>
+      <Button 
+         title="Inscription"
+         type="solid"
+         buttonStyle={{backgroundColor: "#ED590C"}}
+         onPress={() => props.navigation.navigate('API')}
+     />    
+          </View>
+        );
+      }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Input: {
+    width:300
+  }
+});*/
